d60ea22e12d81d9991784a0bada102db04a3486f
Delanie Perez <delanieperez11@gmail.com>
2018-05-10 21:37:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 for (var i = 0; i < 25; i++) {
   if (i === 1) { 
     console.log( "I am 1 strange loop." )
-    
-  else ("I am" + i + "strange loop.")
+  } else {
+    console.log("I am" + i + "strange loop.")
+}
 }
