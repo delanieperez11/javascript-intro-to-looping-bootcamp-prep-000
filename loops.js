4be0def342892b1a9982658df067ad3a08edbd2a
Delanie Perez <delanieperez11@gmail.com>
2018-05-10 21:42:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(array) {
-if (i === 1) { 
+  for (let i = 0; i <25; i++)
+  if (i === 1) { 
     console.log( "I am 1 strange loop." )
   } else {
     console.log("I am" + i + "strange loop.")
